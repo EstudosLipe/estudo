cd3e7a4be61a7b692dfe83d86b1efb33caa24531
Lip22k <131167201+Lip22k@users.noreply.github.com>
2025-07-02 23:16:26 -0300

    Node 07_01
    Atualizações

--- RocketSeat/01.Full-Stack/07.Node/api/src/server.js
+++ RocketSeat/01.Full-Stack/07.Node/api/src/server.js
@@ -3,7 +3,7 @@ import http from "node:http";
 
 // Criação do servidor
 const server = http.createServer((req, res) => {
-    return res.end("Servidor OK xx");
+    return res.end("Servidor Está OK");
 })
 
 server.listen(3333) // Porta mais comum
