3a5f15ba2c75211a3c1acab97a1ce0e81aaf1a61
Lip22k <philipviana122@gmail.com>
2025-07-03 17:08:07 -0300

    Node 07_03

---
+++ RocketSeat/01.Full-Stack/06.JS/projetos/convert/script.js
@@ -0,0 +1,38 @@
+const btn = document.querySelector("button");
+
+const footer = document.querySelector("footer");
+
+function conversionT(valueCoin, conversionCoin) {
+    switch (conversionCoin) {
+        case "USD":
+            arrayCoin = [5.41, valueCoin * 5.41, "US$ "]
+            return arrayCoin
+        case "EUR":
+            arrayCoin = [6.37, valueCoin * 6.37, "€$ "]
+            return arrayCoin
+        case "GBP":
+            arrayCoin = [7.38, valueCoin * 7.38, "£$ "]
+            return arrayCoin
+        default:
+
+    }
+}
+btn.addEventListener("click", () => {
+    try {
+        let valor = document.getElementById("amount").value;
+
+        let conversion = document.querySelector("select").value;
+
+        let precos = conversionT(valor, conversion);
+
+        if (conversion != "" && valor != "" && !isNaN(precos[1])) {
+            footer.style.display = "block";
+            footer.children[0].innerHTML = `${precos[2]}1 = R$ ${precos[0]}`
+            footer.children[1].innerHTML = precos[2] + precos[1].toFixed(2)
+        } else {
+            alert("Digite somente números")
+        }
+    }catch(err){
+        console.log(err)
+    }
+})
--- RocketSeat/01.Full-Stack/07.Node/api/src/server.js
+++ RocketSeat/01.Full-Stack/07.Node/api/src/server.js
@@ -1,9 +1,32 @@
 // Importação com ES Module
 import http from "node:http";
+import { url } from "node:inspector";
 
 // Criação do servidor
-const server = http.createServer((req, res) => {
-    return res.end("Servidor Está OK");
+const server = http.createServer(async (req, res) => {
+
+    // Extrai o método, e URL da requisição
+    const { method, url } = req;
+
+    if (method === "GET" && url === "/products") {
+        return res.writeHead(200).end("Listando produtos")
+    }
+
+    if (method === "POST" && url === "/products") {
+        // Concatenar os chuks
+        const buffers = []
+        for await (const chunk of req) {
+            buffers.push(chunk);
+        }
+
+
+        // Montagem dos chunks (Buffer.concat(buffers))
+        console.log(Buffer.concat(buffers).toString())
+
+        return res.writeHead(201).end("produtos Criados")
+    }
+
+    return res.writeHead(404).end("Não encontrado" + method + url);
 })
 
 server.listen(3333) // Porta mais comum
