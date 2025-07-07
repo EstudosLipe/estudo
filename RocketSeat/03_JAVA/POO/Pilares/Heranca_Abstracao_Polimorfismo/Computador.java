65481af8759c07ed17ebe06ec4f36df14ffdbb74
Lip22k <131167201+Lip22k@users.noreply.github.com>
2025-07-07 05:30:40 -0300

    Estudos sabado

--- RocketSeat/03_JAVA/POO/Pilares/Heranca_Abstracao_Polimorfismo/Computador.java
+++ RocketSeat/03_JAVA/POO/Pilares/Heranca_Abstracao_Polimorfismo/Computador.java
@@ -22,8 +22,6 @@ public class Computador {
                 so.gerenciadoTarefas();
                 break;
             default:
-                
-                
         }
 
         sc.close();
